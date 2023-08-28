c9026dbd11a19af534781595acff07d3287270a4
Prof. Matioli <matioli@unicamp.br>
2023-08-28 14:20:17 -0300

    Incluido botões e controle de status de operações, habilitando e desabilitando controles.

--- BD/frmCliente.Designer.cs
+++ BD/frmCliente.Designer.cs
@@ -29,35 +29,30 @@ namespace BD
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.lstClientes = new System.Windows.Forms.ListBox();
             this.grpDadosCliente = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.idCliente = new System.Windows.Forms.Label();
-            this.Nome = new System.Windows.Forms.Label();
-            this.cnpj_cpf = new System.Windows.Forms.Label();
-            this.txtNome = new System.Windows.Forms.TextBox();
             this.txtCnpjCpf = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.cnpj_cpf = new System.Windows.Forms.Label();
+            this.Nome = new System.Windows.Forms.Label();
+            this.idCliente = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
             this.grpDadosCliente.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(331, 84);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Carregar Clientes (SELECT)";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.cmdSelect);
-            // 
             // lstClientes
             // 
+            this.lstClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstClientes.FormattingEnabled = true;
-            this.lstClientes.Location = new System.Drawing.Point(12, 114);
+            this.lstClientes.ItemHeight = 24;
+            this.lstClientes.Location = new System.Drawing.Point(3, 13);
             this.lstClientes.Name = "lstClientes";
-            this.lstClientes.Size = new System.Drawing.Size(266, 173);
+            this.lstClientes.Size = new System.Drawing.Size(266, 172);
             this.lstClientes.TabIndex = 1;
             this.lstClientes.SelectedIndexChanged += new System.EventHandler(this.cmdSelecionou);
             // 
@@ -71,37 +66,27 @@ namespace BD
             this.grpDadosCliente.Controls.Add(this.label1);
             this.grpDadosCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.grpDadosCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.grpDadosCliente.Location = new System.Drawing.Point(284, 102);
+            this.grpDadosCliente.Location = new System.Drawing.Point(275, 1);
             this.grpDadosCliente.Name = "grpDadosCliente";
             this.grpDadosCliente.Size = new System.Drawing.Size(406, 185);
             this.grpDadosCliente.TabIndex = 2;
             this.grpDadosCliente.TabStop = false;
             // 
-            // label1
+            // txtCnpjCpf
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 24);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "ID CLIENTE:";
+            this.txtCnpjCpf.ForeColor = System.Drawing.Color.Blue;
+            this.txtCnpjCpf.Location = new System.Drawing.Point(11, 134);
+            this.txtCnpjCpf.Name = "txtCnpjCpf";
+            this.txtCnpjCpf.Size = new System.Drawing.Size(209, 29);
+            this.txtCnpjCpf.TabIndex = 5;
             // 
-            // idCliente
+            // txtNome
             // 
-            this.idCliente.AutoSize = true;
-            this.idCliente.Location = new System.Drawing.Point(129, 20);
-            this.idCliente.Name = "idCliente";
-            this.idCliente.Size = new System.Drawing.Size(0, 24);
-            this.idCliente.TabIndex = 1;
-            // 
-            // Nome
-            // 
-            this.Nome.AutoSize = true;
-            this.Nome.Location = new System.Drawing.Point(6, 44);
-            this.Nome.Name = "Nome";
-            this.Nome.Size = new System.Drawing.Size(67, 24);
-            this.Nome.TabIndex = 2;
-            this.Nome.Text = "Nome:";
+            this.txtNome.ForeColor = System.Drawing.Color.Blue;
+            this.txtNome.Location = new System.Drawing.Point(10, 71);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(385, 29);
+            this.txtNome.TabIndex = 4;
             // 
             // cnpj_cpf
             // 
@@ -112,28 +97,100 @@ namespace BD
             this.cnpj_cpf.TabIndex = 3;
             this.cnpj_cpf.Text = "CNPJ / CPF:";
             // 
-            // txtNome
+            // Nome
             // 
-            this.txtNome.Location = new System.Drawing.Point(10, 71);
-            this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(385, 29);
-            this.txtNome.TabIndex = 4;
+            this.Nome.AutoSize = true;
+            this.Nome.Location = new System.Drawing.Point(6, 44);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(67, 24);
+            this.Nome.TabIndex = 2;
+            this.Nome.Text = "Nome:";
             // 
-            // txtCnpjCpf
+            // idCliente
             // 
-            this.txtCnpjCpf.Location = new System.Drawing.Point(6, 134);
-            this.txtCnpjCpf.Name = "txtCnpjCpf";
-            this.txtCnpjCpf.Size = new System.Drawing.Size(209, 29);
-            this.txtCnpjCpf.TabIndex = 5;
+            this.idCliente.AutoSize = true;
+            this.idCliente.ForeColor = System.Drawing.Color.Blue;
+            this.idCliente.Location = new System.Drawing.Point(129, 20);
+            this.idCliente.Name = "idCliente";
+            this.idCliente.Size = new System.Drawing.Size(0, 24);
+            this.idCliente.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID CLIENTE:";
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNovo.Location = new System.Drawing.Point(4, 194);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(111, 36);
+            this.btnNovo.TabIndex = 3;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.cmdNovo);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Location = new System.Drawing.Point(239, 194);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(111, 36);
+            this.btnExcluir.TabIndex = 3;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.cmdExcluir);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(356, 194);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(111, 36);
+            this.btnSalvar.TabIndex = 3;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.cmdSalvar);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(473, 194);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(111, 36);
+            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.cmdCancelar);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(121, 194);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(111, 36);
+            this.btnEditar.TabIndex = 3;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.cmdEditar);
             // 
             // frmCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(702, 300);
+            this.ClientSize = new System.Drawing.Size(676, 238);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnNovo);
             this.Controls.Add(this.grpDadosCliente);
             this.Controls.Add(this.lstClientes);
-            this.Controls.Add(this.button1);
             this.Name = "frmCliente";
             this.Text = "Cadastro de CLIENTES";
             this.grpDadosCliente.ResumeLayout(false);
@@ -143,7 +200,6 @@ namespace BD
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox lstClientes;
         private System.Windows.Forms.GroupBox grpDadosCliente;
         private System.Windows.Forms.TextBox txtCnpjCpf;
@@ -152,6 +208,11 @@ namespace BD
         private System.Windows.Forms.Label Nome;
         private System.Windows.Forms.Label idCliente;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnEditar;
     }
 }
 
--- BD/frmCliente.cs
+++ BD/frmCliente.cs
@@ -9,40 +9,63 @@ namespace BD
     public partial class frmCliente : Form
     {
         DataSet dsCliente;
+        int modo = 0; // 0 = neutro
+
         public frmCliente()
         {
             InitializeComponent();
+            carregaListBox();
+            habilitar();
         }
 
-/*        private void cmdTestaConexao(object sender, EventArgs e)
+        private void habilitar()
         {
-            try
+            if(modo==0)
             {
-                String conString = @"Data Source=(LocalDB)\MSSQLLocalDB;
-                                AttachDbFilename=|DataDirectory|\APP_DATA\Database.mdf; 
-                                Integrated Security=True";
-                SqlConnection con = new SqlConnection(conString);
-                con.Open(); // tenta abrir a conexão
-                MessageBox.Show("Conexão aberta com sucesso!");
-            }catch(SqlException ex)
+                btnNovo.Enabled = true;
+                btnEditar.Enabled = true;
+                btnExcluir.Enabled = true;
+                btnSalvar.Enabled = false;
+                btnCancelar.Enabled = false;
+
+                txtNome.Enabled = false;
+                txtCnpjCpf.Enabled = false;
+            }else if(modo == 1)
             {
-                MessageBox.Show("Falha ao tentar conectar com o BD!\n"+ex.Message);
-            }catch (Exception)
-            {
-                MessageBox.Show("Falha geral do sistema!");
+                btnNovo.Enabled = false;
+                btnEditar.Enabled = false;
+                btnExcluir.Enabled = false;
+                btnSalvar.Enabled = true;
+                btnCancelar.Enabled = true;
+
+                txtNome.Enabled = true;
+                txtNome.Clear();
+                txtCnpjCpf.Enabled = true;
+                txtCnpjCpf.Clear();
+
+                txtNome.Focus(); //posiciona o cursor no campo Nome
             }
+            else if(modo == 2)
+            {
+                btnNovo.Enabled = false;
+                btnEditar.Enabled = false;
+                btnExcluir.Enabled = false;
+                btnSalvar.Enabled = true;
+                btnCancelar.Enabled = true;
+
+                txtNome.Enabled = true;
+                txtCnpjCpf.Enabled = true;
+
+                txtNome.Focus();  //posiciona o cursor no campo Nome
+            }
+
         }
-*/
-        private void cmdSelect(object sender, EventArgs e)
+
+        private void carregaListBox()
         {
-/*
-                 String conString = @"Data Source=(LocalDB)\MSSQLLocalDB;
-                                AttachDbFilename=|DataDirectory|\APP_DATA\Database.mdf; 
-                                Integrated Security=True";
-*/
-                String sqlSelect = "select * from Cliente";
-                SqlConnection con;
-           try
+            String sqlSelect = "select * from Cliente";
+            SqlConnection con;
+            try
             {
                 //Lê a string de conexão do arquivo de configuração da aplicação, App.config
                 String conString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
@@ -52,7 +75,7 @@ namespace BD
                     SqlCommand cmd = new SqlCommand(sqlSelect, con);
                     SqlDataAdapter da = new SqlDataAdapter(cmd);
                     dsCliente = new DataSet();
-                    da.Fill(dsCliente,"Cliente");
+                    da.Fill(dsCliente, "Cliente");
                     DataTable dtCliente = dsCliente.Tables["Cliente"];
 
                     // 1. seta DisplayMember and ValueMember
@@ -70,8 +93,9 @@ namespace BD
             {
                 MessageBox.Show("Falha geral do sistema!");
             }
-        }
 
+        }
+ 
         private void cmdSelecionou(object sender, EventArgs e)
         {
             //String nome="";
@@ -84,8 +108,51 @@ namespace BD
             //Pesquiso o Cliente na DataTable, no DataSet, e crio uma DataRow com os 
             //dados do Cliente
             DataRow[] clientes = dsCliente.Tables["Cliente"].Select("idCliente="+id);
-            if(clientes!=null)
+            if (clientes != null)
+            {
                 Console.WriteLine("{0} - {1} - {2}", clientes[0][0], clientes[0][1], clientes[0][2]);
+                idCliente.Text = clientes[0][0].ToString();
+                txtNome.Text = clientes[0][1].ToString();
+                txtCnpjCpf.Text = clientes[0][2].ToString();
+            }
+        }
+
+        private void cmdEditar(object sender, EventArgs e)
+        {
+            modo = 2; //entra em modo edição
+            habilitar();
+        }
+
+        private void cmdSalvar(object sender, EventArgs e)
+        {
+            //procedimento de persistência em BD
+
+
+            //habilita/desabilita controles
+            modo = 0; //sai do modo edição
+            habilitar();
+ 
+        }
+
+        private void cmdCancelar(object sender, EventArgs e)
+        {
+            //procedimento para cancelar operação corrente
+
+
+            //habilita/desabilita controles
+            modo = 0; //sai do modo atual
+            habilitar();
+
+        }
+
+        private void cmdExcluir(object sender, EventArgs e)
+        {
+            //procedimento para excluir registro corrente
+        }
+
+        private void cmdNovo(object sender, EventArgs e)
+        {
+            //procedimento para incluir novo registro
         }
     }
 }
